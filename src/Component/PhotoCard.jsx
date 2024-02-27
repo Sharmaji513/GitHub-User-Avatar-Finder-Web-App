0c6fd935c7c390aec04c1c0e3605ed45b2234272
sachin <sk800968@gmail.com>
2024-02-27 22:00:31 +0530

    Github Profile Finder Appt

--- src/Component/PhotoCard.jsx
+++ src/Component/PhotoCard.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import "../styles/PhotoCard.css"
+import "./PhotoCard.css"
 
 export const PhotoCard = ({id,url}) => {
     return(
         <div id="photoCard">
+            
             <img src={url} alt="" />
             <h1>{id}</h1>
         </div>
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,41 +1,44 @@
 import React, { useEffect, useState } from "react";
-import "../styles/HomePage.css"
+import "./HomePage.css"
 import { PhotoCard } from "../Component/PhotoCard";
 import useDebounce  from "../customHooks/useDebounce";
 export const HomePage = () => {
-    const[photoData,setphotoData] = useState([])
-    const[query,setQuery]=useState("Farman")
+    const [photoData, setPhotoData] = useState([]);
+    const [query, setQuery] = useState("Sachin sharma");
     const debounceUpdateSearch = useDebounce((e) => setQuery(e.target.value));
-
-    const getData = async() => {
-   
-        try {
-        const resp=await fetch(`https://api.github.com/search/users?q=${query||"Sachin Sharma"}`)
-        const data = await resp.json()
-        setphotoData(data.items)
-
-        } catch (error) {
-          console.log("Error while fetching Data",error.message)  
-        }
-    }
-    const updateQuery=useDebounce()
-
-    useEffect(()=>{
-        getData()
-    },[query])
-    return(
+  
+    const getData = async () => {
+      try {
+        const resp = await fetch(
+          `https://api.github.com/search/users?q=${query}`
+        );
+        const data = await resp.json();
+        setPhotoData(data.items);
+      } catch (error) {
+        console.log("Error while fetching Data", error.message);
+      }
+    };
+  
+    useEffect(() => {
+      getData();
+    }, [query]);
+  
+    return (
+      <div className="homepage-container">
         <div>
-            <div>
-                <input type="text" onChange={debounceUpdateSearch} id="userInput" placeholder="Enter photo Id"/>
-            </div>
-            <div id="imageContainer">
-               {
-                photoData?.map((e)=>{
-                    return <PhotoCard id={e.login} url={e.avatar_url}/>
-                })
-               }
-              
-            </div>
+            <h1 className="heading">Github Profile Search </h1>
+          <input
+            type="text"
+            onChange={debounceUpdateSearch}
+            className="user-input"
+            placeholder="Enter your username"
+          />
         </div>
-    )
-}
+        <div className="image-container">
+          {photoData?.map((e) => {
+            return <PhotoCard key={e.id} id={e.login} url={e.avatar_url} />;
+          })}
+        </div>
+      </div>
+    );
+  };
